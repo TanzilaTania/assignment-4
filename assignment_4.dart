7b66af965eb5cbbd54294fbac57c7f07e25a96b6
unknown <you@example.comtanzilatania31@gmail.com>
2023-05-22 23:11:23 +0600

    Done

---
+++ assignment_4.dart
@@ -0,0 +1,64 @@
+abstract class Account {
+  int accountNumber;
+  double balance;
+
+  Account(this.accountNumber, this.balance);
+
+  void deposit(double amount) {
+    balance += amount;
+    print('Deposited: ${amount.toStringAsFixed(2)}');
+  }
+
+  void withdraw(double amount);
+}
+
+class SavingsAccount extends Account {
+  double interestRate;
+
+  SavingsAccount(int accountNumber, double balance, this.interestRate)
+      : super(accountNumber, balance);
+
+  @override
+  void withdraw(double amount) {
+    if (amount <= balance) {
+      balance -= amount;
+      balance += balance * interestRate;
+      print('Withdrawn: ${amount.toStringAsFixed(2)}');
+    } else {
+      print('Insufficient funds');
+    }
+  }
+}
+
+class CurrentAccount extends Account {
+  double overdraftLimit;
+
+  CurrentAccount(int accountNumber, double balance, this.overdraftLimit)
+      : super(accountNumber, balance);
+
+  @override
+  void withdraw(double amount) {
+    if (amount <= balance + overdraftLimit) {
+      balance -= amount;
+      print('Withdrawn: ${amount.toStringAsFixed(2)}');
+    } else {
+      print('Insufficient funds');
+    }
+  }
+}
+
+void main() {
+
+  SavingsAccount savingsAccount = SavingsAccount(123456, 2000.0, 0.05);
+  print('Savings Account:');
+  savingsAccount.deposit(500.0);
+  savingsAccount.withdraw(3000.0);
+  print('Balance: ${savingsAccount.balance.toStringAsFixed(2)}');
+
+
+  CurrentAccount currentAccount = CurrentAccount(12343, 3000.0, 2000.0);
+  print('\nCurrent Account:');
+  currentAccount.deposit(1000.0);
+  currentAccount.withdraw(500.0);
+  print('Balance: ${currentAccount.balance.toStringAsFixed(2)}');
+}
